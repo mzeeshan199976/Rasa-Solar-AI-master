c5bcca1c5d4e53e98fddf79f043051851d309259
MUHAMMAD ZEESHAN <mzeeshan199976@gmail.com>
2022-12-06 20:59:02 +0500

    changes made in NLU, Stories & domain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 url = 'http://rasa-solar.herokuapp.com/webhooks/rest/webhook' ##change rasablog with your app name
 myobj = {
-"message": "hello",
+"message": "AoA",
 "sender": "Ali",
 }
 x = requests.post(url, json = myobj)
